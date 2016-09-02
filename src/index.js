dc0cbc1ea333fcc4ad0897b200bce42f41e8b58e
leehaoChen <leehaoChen918@163.com>
2016-09-02 17:49:47 +0800

    add route

--- src/index.js
+++ src/index.js
@@ -1,8 +1,50 @@
-import './style/index.css';
-import year from './image/10-years.png'
-console.log("hello world");
-var oimg = document.createElement("img");
-oimg.src = year;
-console.log(year);
-console.log(oimg);
-document.body.appendChild(oimg);
+import React from 'react'
+import ReactDOM from 'react-dom'
+import {
+	Router,
+	Route,
+	hashHistory,
+	Link,
+	Redirect,
+} from 'react-router'
+import Header from './component/header'
+import Home from './component/home'
+import About from './component/about'
+import Curse from './component/curse'
+import Style from './style/main.css'
+const ActiveLink = function(props) {
+	return (<Link activeStyle={{color:"red"}} active {...props}></Link>)
+}
+class App extends React.Component {
+	componentWillMount() {
+		console.log(this.props)
+	}
+	render() {
+		return (<div className="mainBody">	
+		<h1>hi i am leo</h1>
+		<ul>
+			<li><ActiveLink to="/home/ 123">home</ActiveLink></li>
+			<li><ActiveLink to="/about">about</ActiveLink></li>
+		<li><ActiveLink to="/curse">curse</ActiveLink></li>
+			<li><ActiveLink to="/ncurse">ncurse</ActiveLink></li>
+		</ul>
+		{this.props.children}
+		{this.props.sidebar}
+		{this.props.main}
+		</div>)
+	}
+}
+ReactDOM.render(<div className={Style.wrap}>
+	<Header></Header>
+	<Router history={hashHistory}>
+	  <Route path='/' component={App}>
+	   <Route path='home/:id' component={Home} onLeave={(e)=>{console.log(this)}}></Route>
+	   <Route path='about' component={About}></Route>
+	   <Route path='/abouts' components={{main:Home,sidebar:About}}></Route>
+	   <Redirect from="haha" to="/abouts"></Redirect>
+	   <Route path='curse'getComponent={(location,cb)=>{cb(null,Curse)}}></Route> 
+	   <Route path='ncurse' getComponent={(location,cb)=>{cb(null,{sidebar:Curse,main:About})}}></Route>
+	   <Route path='/tiaozhuan' onEnter={(e)=>{}}></Route>
+	  </Route>
+	</Router>
+</div>, document.getElementById('example'))
--- webpack.config.js
+++ webpack.config.js
@@ -47,8 +47,12 @@ module.exports = {
       compress: {
         warnings: false
       }
+    }),
+    new webpack.DefinePlugin({
+      "process.env": {
+        NODE_ENV: JSON.stringify("production")
+      }
     })
-
 
   ],
   /*  devServer: {
